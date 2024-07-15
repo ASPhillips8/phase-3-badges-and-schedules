abf7f92be388b4fd056cebf10429fcc1d12362fa
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-07-15 08:36:51 -0400

    Assign rooms

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,9 @@
 def badge_maker name
   "Hello, my name is #{name}."
 end
+
+def assign_rooms(attendees)
+  attendees.map.with_index(1) do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+end
