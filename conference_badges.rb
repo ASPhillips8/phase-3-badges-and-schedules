516dcdf5a5a4d856f1bf24b62c393a64cace3164
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-07-24 11:56:00 -0400

    Create printer method

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,24 @@ def badge_maker name
   "Hello, my name is #{name}."
 end
 
-def assign_rooms(attendees)
+def batch_badge_creator attendees
+  attendees.map do |name|
+   badge_maker (name)
+  end
+end
+
+def assign_rooms attendees
   attendees.map.with_index(1) do |name, index|
     "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+end
+
+def printer attendees
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
   end
 end
